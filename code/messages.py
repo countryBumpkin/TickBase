df9200ea8bdb8c02aa6dbccc1cb04909763b3e01
countryBumpkin <deepg2w@gmail.com>
2022-01-29 14:30:47 -0800

    Forgot message color definitions in previous commit

---
+++ code/messages.py
@@ -0,0 +1,7 @@
+from colorama import Fore
+
+# define some default message types
+SUCCESS = '[' + Fore.GREEN + 'SUCCESS' + Fore.RESET + ']'
+ERROR = '[' + Fore.RED + 'ERROR' + Fore.RESET + ']'
+WARNING = '[' + Fore.YELLOW + 'WARNING' + Fore.RESET + ']'
+PRMPT = Fore.BLUE
